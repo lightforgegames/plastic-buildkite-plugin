0c13b87abee4f9802203952035ac490965c8a3ea
Dónal Mac Carthy <donal@lightforge.gg>
2022-09-21 17:02:19 +0100

    Set metadata before checkout
    This lets the buildkite UI show the status when updatingrather than waiting until the update is complete, which can be 10 minutes for a clean build.
    Fix some missing newlines, and a linter warning.
    Add a log message upon completion for timestamp goodness.

--- src/main.go
+++ src/main.go
@@ -8,7 +8,7 @@ import (
 	"strconv"
 )
 
-func Shellout(command string) (error, string, string) {
+func Shellout(command string) (string, string, error) {
 	const ShellToUse = "bash"
 	var stdout bytes.Buffer
 	var stderr bytes.Buffer
@@ -16,7 +16,7 @@ func Shellout(command string) (error, string, string) {
 	cmd.Stdout = &stdout
 	cmd.Stderr = &stderr
 	err := cmd.Run()
-	return err, stdout.String(), stderr.String()
+	return stdout.String(), stderr.String(), err
 }
 
 func get_comment(changeset int, branch string) (string, error) {
@@ -26,7 +26,7 @@ func get_comment(changeset int, branch string) (string, error) {
 	} else {
 		query = fmt.Sprintf("log cs:%d --csformat={comment}", changeset)
 	}
-	err, out, _ := Shellout("cm " + query)
+	out, _, err := Shellout("cm " + query)
 	return out, err
 }
 
@@ -41,14 +41,14 @@ func main() {
 		agentId := os.Getenv("BUILDKITE_AGENT_ID")
 
 		workspaceName := fmt.Sprintf("buildkite-%s-%s", pipelineName, agentId)
-		fmt.Printf("Creating workspace %q for repository %q", workspaceName, repoPath)
+		fmt.Printf("Creating workspace %q for repository %q\n", workspaceName, repoPath)
 		out, err := exec.Command("cm", "workspace", "create", workspaceName, ".", "--repository="+repoPath).CombinedOutput()
 		if err != nil {
 			fmt.Printf("Failed to create workspace `%s`: %v.\n%s", workspaceName, err, string(out))
 			os.Exit(1)
 		}
 	} else {
-		fmt.Printf("Cleaning existing workspace...")
+		fmt.Printf("Cleaning existing workspace...\n")
 		if out, err := exec.Command("cm", "undo", ".", "-R").CombinedOutput(); err != nil {
 			fmt.Printf("Failed to undo changes: : %v.\n%s", err, string(out))
 			os.Exit(1)
@@ -66,32 +66,34 @@ func main() {
 	if !(revision == "" || revision == "HEAD") {
 		// If the revision isn't empty, or head, then set the target to the specified changeset
 		if changeset, err = strconv.Atoi(revision); err != nil || changeset < 1 {
-			fmt.Printf("Invalid changeset specified. Expected a numeric value but got `%s`", revision)
+			fmt.Printf("Invalid changeset specified. Expected a numeric value but got `%s`\n", revision)
 			os.Exit(1)
 		}
 	}
 
 	if len(target) == 3 {
-		fmt.Printf("Invalid target, expected either a branch or a changeset but got `%s`", target)
+		fmt.Printf("Invalid target, expected either a branch or a changeset but got `%s`\n", target)
+		os.Exit(1)
+	}
+
+	// Set metadata before updating, as updating can take minutes.
+	comment, err := get_comment(changeset, branch)
+	if err != nil {
+		fmt.Printf("Failed to get comment for `%v:%s`\n", changeset, branch)
+		fmt.Printf("Failed to get comment: : %v.\n%s\n", err, comment)
+		os.Exit(1)
+	}
+	commitMetadata := fmt.Sprintf("commit %s\n\n\t%s", revision, comment)
+	if out, err := exec.Command("buildkite-agent", "meta-data", "set", "buildkite:git:commit", commitMetadata).CombinedOutput(); err != nil {
+		fmt.Printf("Failed to set metadata: : %v.\n%s\n", err, string(out))
 		os.Exit(1)
 	}
 
 	fmt.Println("Setting workspace to " + target)
 	if out, err := exec.Command("cm", "switch", target).CombinedOutput(); err != nil {
-		fmt.Printf("Failed to update workspace: : %v.\n%s", err, string(out))
+		fmt.Printf("Failed to update workspace: : %v.\n%s\n", err, string(out))
 		os.Exit(1)
 	}
 
-	// Finally, set metadata
-	comment, err := get_comment(changeset, branch)
-	if err != nil {
-		fmt.Printf("Failed to get comment for `%v:%s`", changeset, branch)
-		fmt.Printf("Failed toget comment: : %v.\n%s", err, comment)
-		os.Exit(1)
-	}
-	commitMetadata := fmt.Sprintf("commit %s\n\n\t%s", revision, comment)
-	if out, err := exec.Command("buildkite-agent", "meta-data", "set", "buildkite:git:commit", commitMetadata).CombinedOutput(); err != nil {
-		fmt.Printf("Failed to set metadata: : %v.\n%s", err, string(out))
-		os.Exit(1)
-	}
+	fmt.Println("Update complete.")
 }
